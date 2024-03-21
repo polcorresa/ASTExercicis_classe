baf194371676cc4a79ed68892d4ad2ffe221da77
Pol Corresa <polcorresa@10-192-189-217client.eduroam.upc.edu>
2024-03-21 17:48:34 +0100

    Broadcast

--- src/main/java/E8Broadcast_assignment/Broadcast.java
+++ src/main/java/E8Broadcast_assignment/Broadcast.java
@@ -4,10 +4,57 @@
  */
 package E8Broadcast_assignment;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *
  * @author polcorresa
  */
 public class Broadcast {
-    
+
+    protected Object espai;
+
+    protected boolean[] disponible;
+    protected boolean[] isDisponible;
+    protected Lock mon = new ReentrantLock();
+    protected Condition sent = mon.newCondition();
+    protected Condition taken = mon.newCondition();
+
+    public Broadcast(int N) {
+
+        for (int i = 0; i < N; i++) {
+            disponible[i] = true;
+            isDisponible[i] = false;
+        }
+
+    }
+
+    public void putValue(Object objecte) {
+        mon.lock();
+
+        while (!disponible.equals(isDisponible)) {
+            sent.awaitUninterruptibly();
+        }
+        for (int i = 0; i < disponible.length; i++) {
+            disponible[i] = true;
+        }
+        espai = objecte;
+        taken.signalAll();
+        mon.unlock();
+    }
+
+    public Object getValue(int i) {
+        mon.lock();
+        while (disponible[i] == false) {
+            taken.awaitUninterruptibly();
+        }
+        disponible[i] = false;
+        sent.signal();
+        Object objecte = espai;
+        mon.unlock();
+        return objecte;
+    }
+
 }
