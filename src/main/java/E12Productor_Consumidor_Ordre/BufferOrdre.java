3d79d8a2dd9c19408dd8c535a2fb44d76bbe6ec7
Pol Corresa <polcorresa@Pols-Air-3.home>
2024-05-18 17:22:42 +0200

    Ns que era

--- src/main/java/E12Productor_Consumidor_Ordre/BufferOrdre.java
+++ src/main/java/E12Productor_Consumidor_Ordre/BufferOrdre.java
@@ -4,10 +4,55 @@
  */
 package E12Productor_Consumidor_Ordre;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  *
  * @author polcorresa
  */
 public class BufferOrdre {
-    
+
+    protected Lock mon;
+    protected ArrayList<Condition> condicionsOrdre;
+    protected ArrayList<Object> ordreObjecte;
+    protected int waiting;
+
+    public void put(Object element) {
+        try {
+            mon.lock();
+
+            ordreObjecte.add(element);
+            if (waiting > 1) {
+                condicionsOrdre.get(0).signal();
+            }
+
+        } finally {
+            mon.unlock();
+        }
+
+    }
+
+    public Object get() {
+        try {
+            mon.lock();
+
+            Condition myCond = mon.newCondition();
+            condicionsOrdre.add(myCond);
+            waiting++;
+            while (waiting > 1 || ordreObjecte.isEmpty()) {
+                myCond.awaitUninterruptibly();
+            }
+            waiting--;
+
+            //Em toca agafar
+            condicionsOrdre.remove(0);
+            Object myObj = ordreObjecte.get(0);
+            return myObj;
+
+        } finally {
+            mon.unlock();
+        }
+    }
 }
